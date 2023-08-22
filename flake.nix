6c284c505a544d69312b684b7a5a5f864106beda
Ariel Richtman <Ariel.Richtman@SilverRailTech.com>
2023-08-22 11:16:01 +1000

    added welcome text

--- flake.nix
+++ flake.nix
@@ -9,6 +9,20 @@
       pe = {
         path = ./pe;
         description = "Platform Engineering environment";
+        welcomeText = ''
+          # SilverRailTech Platform Engineering Environment
+
+          ## Inclusions
+
+          - Pre-commit hooks
+          - Nix flake development environment
+          - Python + Poetry project
+          - Git attributes
+          - Git ignore
+          - Direnv activation file
+
+          glhf
+        '';
       };
     };
   };
