1ae062268ad834178473552ab4534218bf8b9586
Ariel Richtman <Ariel.Richtman@SilverRailTech.com>
2024-02-21 16:46:44 +1000

    standardized pe pre-commit

--- copy-repeated-files.sh
+++ copy-repeated-files.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find . -maxdepth 1 -type d -not -path '*/.*' -not -path '.'  | xargs -I% cp .envrc .pre-commit-config.yaml %
-find . -maxdepth 1 -type d -not -path '*/.*' -not -path '.'  | xargs -I% git add --force %/.envrc %/.pre-commit-config.yaml
+find . -maxdepth 1 -type d -not -path '*/.*' -not -path '.' -not -path './pe'  | xargs -I% cp .envrc .pre-commit-config.yaml %
+find . -maxdepth 1 -type d -not -path '*/.*' -not -path '.' -not -path './pe'  | xargs -I% git add --force %/.envrc %/.pre-commit-config.yaml
